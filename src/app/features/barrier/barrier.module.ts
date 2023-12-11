5e9ee44c601cb33e50ca95687eb712b30e73243f
hugo.herrador <hugo.herrador@inetum.com>
2023-12-11 11:40:28 +0100

    finish with the shared lazy loaded module

--- src/app/features/barrier/barrier.module.ts
+++ src/app/features/barrier/barrier.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarrierComponent } from './barrier.component';
+import { BarrierRoutingModule } from './barrier-routing.module'
 
 
 
@@ -9,7 +10,8 @@ import { BarrierComponent } from './barrier.component';
     BarrierComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BarrierRoutingModule
   ]
 })
 export default class BarrierModule { }
--- src/app/features/visor/visor.module.ts
+++ src/app/features/visor/visor.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VisorRoutingModule } from './visor-routing.module';
+import { VisorComponent } from './visor.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    VisorComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    VisorRoutingModule
   ]
 })
 export default class VisorModule { }
