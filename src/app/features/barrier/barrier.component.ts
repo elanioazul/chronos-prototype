6daafe05692c1c69a3fa04288db5d6dd2fbb7e7a
Hugo Herrador <hugo.herrador@inetum.com>
2024-01-11 12:09:59 +0100

    pass visor config to state

--- src/app/features/barrier/barrier.component.ts
+++ src/app/features/barrier/barrier.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
+import { mockVisor } from '@features/visor/core/consts/config-visor';
+import { VisorService } from '@features/visor/core/services/visor.service';
 @Component({
   selector: 'app-barrier',
   templateUrl: './barrier.component.html',
   styleUrls: ['./barrier.component.scss']
 })
-export class BarrierComponent {
+export class BarrierComponent implements OnInit {
+
+  visorServ = inject(VisorService);
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    this.visorServ.readVisorConfig$.next(mockVisor)
+  }
 
 }
