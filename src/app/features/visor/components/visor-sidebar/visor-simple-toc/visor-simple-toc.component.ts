d42b5d1e756262c6d240e535834463baf32fd444
Hugo Herrador <hugo.herrador@inetum.com>
2024-01-22 15:29:54 +0100

    add TODO related to zIndex and dragAnddrop

--- src/app/features/visor/components/visor-sidebar/visor-simple-toc/visor-simple-toc.component.ts
+++ src/app/features/visor/components/visor-sidebar/visor-simple-toc/visor-simple-toc.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { MapService } from '@features/visor/core/services/map.service';
+import { TooltipModule } from 'primeng/tooltip';
 @Component({
   selector: 'app-visor-simple-toc',
   templateUrl: './visor-simple-toc.component.html',
@@ -13,23 +14,28 @@ export class VisorSimpleTocComponent {
 
 
   public dropLayer(event: any/*CdkDragDrop<string[]>*/) {
-		moveItemInArray(
-			this.tocLyrs(),
-			event.previousIndex,
-			event.currentIndex
-		);
-		let init: number;
-		let end: number;
-		if (event.previousIndex > event.currentIndex) {
-			init = event.currentIndex;
-			end = event.previousIndex;
-		} else {
-			init = event.previousIndex;
-			end = event.currentIndex;
-		}
-		for (init; init <= end; init++) {
-			const zIndex = this.tocLyrs().length - init;
-			this.tocLyrs()[init].zIndex = zIndex;
-		}
+	/*
+		TODO: esto dropLayer tiene que actualizar el state (la signal simpleTocLayers)
+		para lo cual habrá que hacer una source de entrada y lo que no se es si
+		habrá que sobreescribir los zindex que el state tiene con los que aporta este evento o podré buscar correspondencias
+	*/
+	moveItemInArray(
+		this.tocLyrs(),
+		event.previousIndex,
+		event.currentIndex
+	);
+	let init: number;
+	let end: number;
+	if (event.previousIndex > event.currentIndex) {
+		init = event.currentIndex;
+		end = event.previousIndex;
+	} else {
+		init = event.previousIndex;
+		end = event.currentIndex;
 	}
+	for (init; init <= end; init++) {
+		const zIndex = this.tocLyrs().length - init;
+		this.tocLyrs()[init].zIndex = zIndex;
+	}
+}
 }
